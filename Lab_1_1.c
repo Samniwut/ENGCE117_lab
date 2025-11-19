3f3462fc21068340a9ba247fae20542626faae49
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-19 12:37:50 +0700

    addcode lab1-1

--- Lab_1_1.c
+++ Lab_1_1.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
-int main() {
-    
-    printf("Hello, World!\n");
-    
+#include <stdlib.h>
+int main()
+{
+    int N, i;
+    scanf("%d",&N);
+    if( N <= 0){
+        printf("Erorr");
+    }else{
+    for(i=0; i<N ;i++){
+        printf("Hello Loop!\n");
+    }
+    }
     return 0;
 }
