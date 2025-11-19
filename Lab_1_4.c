1a249c872c71f69f4390d45f5a03b6fed7b2ea10
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-19 12:46:32 +0700

    addlab1-4

---
+++ Lab_1_4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int N, sum = 0, i;
+   scanf("%d",&N);
+   if(N<0){
+    printf("Erorr");
+    return 0;
+   }
+   for(i=0; i<=N ; i++){
+    sum = sum+i;
+   }
+   printf("%d",sum);
+    return 0;
+}
