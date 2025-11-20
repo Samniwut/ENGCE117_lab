429234612d48fc7cd846c5e9b36245c82b4869c4
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-20 13:34:04 +0700

    Edit lab-1.4.2

--- Lab_1_4.c
+++ Lab_1_4.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+
+int calculatesum(int N);
 int main()
 {
-    int N, sum = 0, i;
-   if(scanf("%d",&N) || N<=0){
-    printf("Erorr");
+    int N, sum = 0 ;
+   if(scanf("%d",&N)!= 1 || N<=0){
+    printf("Error");
     return 1;
-   }
-   for(i=0; i<=N ; i++){
-    sum +=i;
+   }else{
+     sum = calculatesum(N);
    }
    printf("%d",sum);
     return 0;
+}
+int calculatesum(int N){
+    int resultsum = 0;
+    for(int i=0; i<=N ;i++){
+    resultsum +=i;
+   }
+   return resultsum;
 }
