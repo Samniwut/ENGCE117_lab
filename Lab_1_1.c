ce943bbf5de0e37fd4d6a9dcffd1a6f258f3a171
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-20 13:00:54 +0700

    Edit lab1-1

--- Lab_1_1.c
+++ Lab_1_1.c
@@ -1,14 +1,22 @@
+// Pre-code (โค้ดบังคับ)
 #include <stdio.h>
-int main()
-{
-    int N, i;
-    scanf("%d",&N);
-    if( N <= 0){
-        printf("Erorr");
+
+int main() {
+    int N;
+
+    // รับค่า N (จำนวนรอบ)
+    if (scanf("%d", &N) != 1) {
+            printf("Error");
+        return 0;// Handle input failure
+    }
+    if(N<=0){
+        printf("Error");
     }else{
-    for(i=0; i<N ;i++){
+      for(int i=0; i<N ;i++){
         printf("Hello Loop!\n");
     }
     }
+    // TODO: Implement the for loop to print the message N times
+
     return 0;
 }
