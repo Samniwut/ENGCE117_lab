fd7d9c9b49a962a30dc4c1734f9acd1bd7fcaba3
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-20 14:00:21 +0700

    Edit lab-1.3.2

--- Lab_1_3.c
+++ Lab_1_3.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
-
+int print_result(int result);
 int main() {
     int num1, num2, operationCode, result;
      if (scanf("%d %d %d", &num1, &num2, &operationCode) != 3) {
         printf("Invalid Operation");
-        return 0;} // Handle input failure
-    if(operationCode >= 5){
-        printf("Invalid Operation");
-        return 0;
-    }
+        return 1;} // Handle input failure
+
     switch(operationCode){
         case 1:
         result=num1+num2;
+        print_result(result);
         break;
         case 2:
           result=num1-num2;
+          print_result(result);
         break;
         case 3:
           result=num1*num2;
+          print_result(result);
         break;
         case 4:
           result=num1/num2;
+          print_result(result);
         break;
+        default:
+          printf("Invalid Operation");
+          return 0;
     }
-    printf("%d",result);
     return 0;
 }
+int print_result(int result){
+    printf("%d",result);
+}
+
