2a808abfb2f4d6043f3ccc0040d8adee6b95a878
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-23 00:38:45 +0700

    edit and add fucnsion

--- Lab_2_1.c
+++ Lab_2_1.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+int printHelloLoop(int N);
    int main() {
     int N, i;
   scanf("%d",&N);
     if (N <= 0){
         printf("Error");
     }else{
-        for (int i = 0; i < N; i++){
-            printf("Hello Loop!\n");
-        }
+        printHelloLoop(N);
     }
     return 0;
+}
+int printHelloLoop(int N){
+ for (int i = 0; i < N; i++){
+            printf("Hello Loop!\n");
+        }
 }
