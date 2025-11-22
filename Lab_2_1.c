3e169acd1588f3d4477f91aaf5bcd10b113515fd
Samniwut <samkonmueng_sk@hotmail.com>
2025-11-22 12:49:57 +0700

    uploadfile lab 2.1.1

--- Lab_2_1.c
+++ Lab_2_1.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
    int main() {
-
     int N, i;
-
-    if (scanf("%d", &N) != 1 || N <= 0) {
+  scanf("%d",&N);
+    if (N <= 0){
         printf("Error");
-        return 1;
-    }
-    if (N > 0){
+    }else{
         for (int i = 0; i < N; i++){
             printf("Hello Loop!\n");
         }
     }
     return 0;
 }
-
-
