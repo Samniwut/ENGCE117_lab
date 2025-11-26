0d41faba83b5a793bafd4d418ac119fd555a9d17
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-26 16:49:57 +0700

    Editfile Lab2.9

--- Lab_2_9.cpp
+++ Lab_2_9.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void CurrentStockPrintf(int currentStock , float totalPenalties);
 int main(){
 
     int initialStock , N , cmdCode, quantity, i;
@@ -6,16 +7,47 @@ int main(){
     int currentStock;
     float totalPenalties = 0.0;
 
-    if (scanf("%d %d %f",&initialStock, &N, &PENALTY_FEE) != 3){
+    if (scanf("%d %f %d",&initialStock, &PENALTY_FEE, &N) != 3){
         return 1;
     }
-    currentStock = initialStock;  
 
+    currentStock = initialStock;  
+    
     for(i = 0; i < N; i++){
+
         if(scanf("%d %d",&cmdCode, &quantity) != 2){
             break;
         }
-    }
 
+        switch (cmdCode)
+        {
+        case 1:
+            currentStock += quantity;
+            printf("Received %d units\n", quantity);
+            break;
+        case 2:
+            if(quantity <= 0){
+                printf("Error : Quantity %d units\n", quantity);
+            }else if(quantity <= currentStock){
+                printf("Shipped %d units\n", quantity);
+                currentStock -= quantity;
+            }else if(quantity > currentStock){
+                totalPenalties += PENALTY_FEE;
+                printf("FAILURE : Insufficient Stock. FENALTY %.2f applied.\n",PENALTY_FEE);
+            }
+            break;
+        case 3:
+            CurrentStockPrintf( currentStock , totalPenalties);
+            break;
+        default:
+        printf("Error : Invalid Command.\n");
+            break;
+        }
+    }
     return 0;
+}
+
+void CurrentStockPrintf(int currentStock , float totalPenalties){
+    printf("Current Stock: %d\n", currentStock);
+    printf("Total Penalties: %.2f\n", totalPenalties);
 }
