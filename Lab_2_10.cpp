9b69bd0a79ad25521f19ff77969db6e0c698bc75
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-26 16:28:38 +0700

    Editfile Lab2.12

--- Lab_2_10.cpp
+++ Lab_2_10.cpp
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
+void currentBalancePrintf(float currentBalance);
+void printFinalAndTotal(float currentBalance, float totalPenalties);
+
+
 int main(){
     int N_PERIODS, cmdCode, i;
     float initialBalance ,PRMALTYY_FEE, amount, interestRate = 0.0; 
     float currentBalance ; 
-    float totalPenalties = 0.0 ; 
+    float totalPenalties = 0.0 , APR = 0.0; 
 
     if (scanf("%f %f %d", &initialBalance,&PRMALTYY_FEE,&N_PERIODS) != 3) {
         return 1;
     }   
     currentBalance = initialBalance;
-    printf("Starting Balance: %.2f\n", currentBalance);
+    currentBalancePrintf(currentBalance);
 
     for ( i = 0; i < N_PERIODS; i++)
     {  
@@ -25,25 +29,21 @@ int main(){
             break;
         case 2:
             if(amount <= currentBalance){
-                //currentBalance -= amount;
-                printf("Withdrawal of %.2f successful. Current balance: %.2f\n", amount, currentBalance);
+                printf("WITHDRAWAL of %.2f successful.\n", amount);
+                currentBalance -= amount;
             }else{
                 totalPenalties += PRMALTYY_FEE;
                 printf("WITHDRAWAL FAILED. Penalty %.2f applied .\n", PRMALTYY_FEE);
                 }
             break;
         case 3:
-            if(currentBalance >= 1000.0){
-                const float APR_HIGH = 2.5 / 100;
-                interestRate = initialBalance * (APR_HIGH/10);
-                
-            }else{
-                const float APR_LOW = 1.0 / 100;
-                interestRate = initialBalance * (APR_LOW/10);
-                     
+            if(currentBalance >= 1000.00){
+                APR = 2.5 ;
+            }else if(currentBalance < 1000){
+                APR = 1.0 ;
             }
-              currentBalance += interestRate;
-                printf("Interest %.2f (Rate : %.2f%%) \n", interestRate,APR_HIGH*100); 
+              currentBalance += (interestRate = (currentBalance * (APR / 100) / 12));
+                printf("Interest %.2f (Rate : %.2f%%) \n", interestRate,APR); 
             break;
             
         default:
@@ -51,10 +51,13 @@ int main(){
         break;
         }
         }
-
-    // Final balance calculation
-
+    printFinalAndTotal(currentBalance,totalPenalties);
+    return 0;
+}
+void currentBalancePrintf(float currentBalance){
+    printf("Starting Balance: %.2f\n", currentBalance);
+}
+void printFinalAndTotal(float currentBalance, float totalPenalties){
     printf("Final Balance: %.2f\n", currentBalance);
     printf("Total Penalties: %.2f\n", totalPenalties);
-    return 0;
 }
