663ec9dddd8a1f19ea69ec540f7e83acc992c873
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-28 14:58:15 +0700

    edit_v1

--- ComputerEngineer_Lab/Lab_1_2.cpp
+++ ComputerEngineer_Lab/Lab_1_2.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-void Getset(int **date, int *num);
+void Getset(int *date[5], int *num);
 int main() {
     int *date , num;
     scanf("%d %d",&date,&num);
-    Getset(&date, &num);
+    printf("%d %d",date,num);
+    //Getset(&date, &num);
     return 0;
 }
 
-void Getset(int **date, int *num){
-    for(int i=0; i<**date; i++){
-        printf("%d ", date[i]);
+void Getset(int *date[5], int *num){
+    for(int i=0; i<*num; i++){
+        printf("%d ", *num);
     }
     
 }
