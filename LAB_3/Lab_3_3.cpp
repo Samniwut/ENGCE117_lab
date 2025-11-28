fed0f1931ed06d4b9cbd5af96f99aaa90ba907fd
Sam lenovo <samkonmueng_sk@hotmail.com>
2025-11-28 09:42:36 +0700

    uploadfile Lab-3-3

---
+++ LAB_3/Lab_3_3.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+float avgcalcollection(float total, int N);
+
+struct Student{
+    char name[50];
+    int StudentId;
+    float score;
+};
+
+int main(){
+
+  int N,i ; 
+  float total=0.0, avg; 
+if(scanf("%d", &N) != 1){
+    return 1;   }
+
+struct Student students[N];
+for (i = 0; i < N; i++){
+    if(scanf("%d %f %s", &students[i].StudentId, &students[i].score ,&students[i].name) != 3){
+        return 1;}
+}
+for ( i = 0; i < N; i++)
+{
+    total += students[i].score;
+}
+    avg = avgcalcollection(total, N);
+
+     printf("Average Score: %.2f\n", avg);
+    return 0;
+}
+
+float avgcalcollection(float total, int N){
+    float avg;
+    avg = total / N;
+    return avg;
+}
