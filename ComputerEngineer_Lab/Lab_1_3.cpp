05df187edad8a08c74df62e2b2cc1564f428562f
Samniwut <samkonmueng_sk@hotmail.com>
2025-11-30 12:53:22 +0700

    updateLab1-3v2

--- ComputerEngineer_Lab/Lab_1_3.cpp
+++ ComputerEngineer_Lab/Lab_1_3.cpp
@@ -2,13 +2,14 @@
 int *Getset(int *arr);
 
 int main() {
-    int *data,num;
-    scanf("%d", &num);
-
-    data = Getset(&num);
-    printf("data : %d",*data);
+    int *data,number;
+    if (scanf("%d", &number) != 1){   
+        return 0;
+    }
+    data = Getset(&number);
+    printf("%d",*data);
     return 0;
 }
 int *Getset(int *arr){
-    return arr;
+    return arr + 2;
 }
