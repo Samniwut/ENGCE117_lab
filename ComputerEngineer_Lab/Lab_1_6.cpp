f062ff7c7f3e5bdca6fef40d05b609a04753a8b9
Samniwut <samkonmueng_sk@hotmail.com>
2025-12-01 01:05:47 +0700

    Fix GetMatrix function signature and improve input handling in Lab_1_6.cpp

--- ComputerEngineer_Lab/Lab_1_6.cpp
+++ ComputerEngineer_Lab/Lab_1_6.cpp
@@ -1,24 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-int GetMatrix(int *row , int *col);
+int *GetMatrix(int *row , int *col);
 
 int main(){
-    int *data , m,n ;
-    scanf("%d %d",&m,&n);
+    int *data , m, n ;
     data = GetMatrix(&m,&n);
+    if (data == NULL) {
+        return 1;
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", data[i * n + j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
-
-int GetMatrix(int *row , int *col){
+int *GetMatrix(int *row , int *col){
     int *a ;
-    printf("How many row col ?");
-    scanf("%d %d", row , col);
-    a = (int *)malloc(sizeof(int)*(*row * *col));
-    for( int i = 0 ; i < *row ; i++){
-       for(int j = 0 ; j < *col ; i++){
-        scanf("%d" ,&a[i * *col + j]);
+    printf("How many row col ? ");
+    if (scanf("%d %d", row , col) != 2) {
+        return 1 ;
+    }
+    a = (int *)malloc(sizeof(int) * (*row * *col));
+
+    for (int i = 0; i < *row; i++){
+       for (int j = 0; j < *col; j++){
+           if (scanf("%d", &a[i * (*col) + j]) != 1) {
+               return 1;
+           }
        }
     }
-    return *a ;
+    return a ;
 }
