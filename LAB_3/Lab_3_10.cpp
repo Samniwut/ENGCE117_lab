9154876722c4340a554198c9c8602d6f86378a03
Samniwut <samkonmueng_sk@hotmail.com>
2025-12-03 00:19:45 +0700

    Refactor Lab_3_10.cpp to improve code structure and readability by rearranging function declaration and removing unnecessary lines

--- LAB_3/Lab_3_10.cpp
+++ LAB_3/Lab_3_10.cpp
@@ -5,7 +5,7 @@ struct Item{
     int quanatity ;
     
 };
-
+  void printfLine(float subtotal, float totalVat,float gradTotal);
 
 int main(){
     
@@ -13,35 +13,28 @@ int main(){
     float VAT_RATE = 0.07 ;
     float totalVat = 0.0 ,gradTotal =0.0, subtotal = 0.0 ;
 
-
     if(scanf("%d",&N) != 1){
         return 1 ;
      }
    struct Item Item[N];
-
      for(i = 0; i <N ; i++){
       float ItemCost = 0.0;
-      ;
-      
        if(scanf("%s %f %d",&Item[i].name,&Item[i].unitPrice,&Item[i].quanatity) !=3){
         return 1 ;
        }
       ItemCost = Item[i].unitPrice*Item[i].quanatity;
        printf("Name : %s x %d  = %.2f\n",Item[i].name,Item[i].quanatity,ItemCost);
-     
        subtotal += ItemCost;
-    }
-    
+    } 
     totalVat = subtotal * VAT_RATE;
     gradTotal =  subtotal+totalVat;
-
-    printf("SubTotal : %.2f\n",subtotal);
-    printf("VAT (7%) : %.2f \n",totalVat);
-    printf("Grand Total : %.2f\n",gradTotal);
-
-
-
+    printfLine(subtotal, totalVat,gradTotal);
    
     return 0;
 
+}
+void printfLine(float subtotal, float totalVat,float gradTotal){
+    printf("SubTotal : %.2f\n",subtotal);
+    printf("VAT (7%) : %.2f \n",totalVat);
+    printf("Grand Total : %.2f\n",gradTotal);
 }
