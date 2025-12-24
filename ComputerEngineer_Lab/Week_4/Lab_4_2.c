06e176f563d430ab1ecc9eacee1cd80d18d1ece4
Sam_Mac <samkonmueng_sk@hotmail.com>
2025-12-24 12:39:14 +0700

    editfile Lab4-2

---
+++ ComputerEngineer_Lab/Week_4/Lab_4_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ void go(int ***p, int **z) {
+    *p = z;
+ }
+
+int main() {
+    int *b = (int *)malloc(sizeof(int));
+    int *c = (int *)malloc(sizeof(int));
+    *b = 10;
+    *c = 20;
+    int **a;
+    printf("\n");
+    go(&a, &b);
+    printf("%d %p %p \n", **a,*a,&a);
+    printf("-----\n");
+    go(&a, &c);
+    printf("%d %p %p \n", **a,*a,&a);
+    return 0;
+}
