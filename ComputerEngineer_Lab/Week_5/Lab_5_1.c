63fe1dd4814ed37683ff8d4688bd36f870352d07
Sam_Mac <samkonmueng_sk@hotmail.com>
2026-01-05 18:21:32 +0700

    addlab5-1

--- ComputerEngineer_Lab/Week_5/Lab_5_1.c
+++ ComputerEngineer_Lab/Week_5/Lab_5_1.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ struct studentNode{
+   char name[50];
+   int age;
+   char sex;
+   float gpa;
+ struct studentNode* next;
+ };
+typedef struct studentNode studentNode;
+
+void ShowAll(studentNode *walk){
+  while(walk != NULL){
+    printf("%s -> ", walk->name);
+    walk = walk->next;
+  }
+  printf("\n");
+}
+
 int main() {
-  
+  studentNode *start, *now;
+  start = NULL;
+studentNode s1, s2, s3;
+strcpy(s1.name, "one");
+strcpy(s2.name, "two");
+strcpy(s3.name, "three");
+start = &s1;
+s1.next = &s2;
+s2.next = &s3;
+s3.next = NULL;
+
+ShowAll(start);
+//   now = AddNode(&start, "one"); ShowAll(start);
+//   now = AddNode(&start, "two"); ShowAll(start);
     return 0;
 }
